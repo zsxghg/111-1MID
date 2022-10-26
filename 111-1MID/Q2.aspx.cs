64c3fa66833a57f3dd68742d52ac6d913e083f1b
altoliaw <altoliaw@hotmail.com>
2022-10-26 13:36:41 +0800

    修改項目

--- 111-1MID/Q2.aspx.cs
+++ 111-1MID/Q2.aspx.cs
@@ -7,10 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace _111_1MID {
     public partial class Q2 : System.Web.UI.Page {
-        string[] s_City = new string[2] { "台北市", "新北市" };
-        string[,] s_Area = new string[2, 3] {
+        string[] s_City = new string[3] { "台北市", "新北市", "台中市" };
+        string[,] s_Area = new string[3, 3] {
             {"中正區", "文山區", "大安區"},
-            {"淡水區", "石碇區", "土城區"}
+            {"淡水區", "石碇區", "土城區"},
+            {"西屯區", "北屯區", "東區"}
         };
 
         protected void Page_Load(object sender, EventArgs e) {
